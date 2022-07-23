632152b7656a7e75c4f02021e818c1c6420954a8
ultroi <86416271+ultroi@users.noreply.github.com>
2022-07-23 14:37:33 +0530

    Update userbot_tools.py

--- program/userbot_tools.py
+++ program/userbot_tools.py
@@ -3,7 +3,7 @@ from driver.veez import user
 from pyrogram.types import Message
 from pyrogram import Client, filters
 from config import BOT_USERNAME, SUDO_USERS
-from driver.filters import command, other_filters
+from driver.filters import command, filters_ex, filters_in
 from pyrogram.errors import UserAlreadyParticipant
 from driver.decorators import authorized_users_only, sudo_users_only
 
