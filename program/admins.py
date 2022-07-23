302f0af4036aea542fd596a0fbbad86db2f04243
ultroi <86416271+ultroi@users.noreply.github.com>
2022-07-23 14:33:05 +0530

    Update admins.py

--- program/admins.py
+++ program/admins.py
@@ -2,7 +2,7 @@ from cache.admins import admins
 from driver.veez import call_py
 from pyrogram import Client, filters
 from driver.decorators import authorized_users_only
-from driver.filters import command, other_filters
+from driver.filters import command, filters_ex, filters_in
 from driver.queues import QUEUE, clear_queue
 from driver.utils import skip_current_song, skip_item
 from config import BOT_USERNAME, GROUP_SUPPORT, IMG_3, UPDATES_CHANNEL
